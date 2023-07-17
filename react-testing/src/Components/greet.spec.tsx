137b7b0a1fb75b0050f1d647c17d8b68c06472ae
oauth2 <abhishek.singh@stashfin.com>
2023-07-17 15:39:05 +0530

    watch mode , filterring test, grouping tests, filename convention

--- react-testing/src/Components/greet.spec.tsx
+++ react-testing/src/Components/greet.spec.tsx
@@ -4,18 +4,19 @@ import Greet from "./greet";
 //TDD(Test driven deployment --> here we write the test cases before the code has to be written)
 // Greet component should render the text hello and if the name component is passed then it should render the text hello folowed by name
 
+//test === it
+//To compile test.only === fit
+//To compile test test.skip === xit
 describe("Greet", () => {
-	test("rendered successfully", () => {
+	it("rendered successfully", () => {
 		render(<Greet />);
 		const textElement = screen.getByText(/hello/i);
 		expect(textElement).toBeInTheDocument();
 	});
 
-	describe("Nested", () => {
-		test("renders with names", () => {
-			render(<Greet name="abhishek" />);
-			const textElement = screen.getByText("Hello abhishek");
-			expect(textElement).toBeInTheDocument();
-		});
+	it("renders with names", () => {
+		render(<Greet name="abhishek" />);
+		const textElement = screen.getByText("Hello abhishek");
+		expect(textElement).toBeInTheDocument();
 	});
 });
--- react-testing/src/__tests__/greet-test.tsx
+++ react-testing/src/__tests__/greet-test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Greet from "./greet";
+import Greet from "../Components/greet";
 
 //TDD(Test driven deployment --> here we write the test cases before the code has to be written)
 // Greet component should render the text hello and if the name component is passed then it should render the text hello folowed by name
