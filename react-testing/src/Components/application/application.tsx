b25213d2d7e0ba39aa175286ed9dd21313bcc730
oauth2 <abhishek.singh@stashfin.com>
2023-07-18 15:06:36 +0530

    rtl queries

--- react-testing/src/Components/application/application.tsx
+++ react-testing/src/Components/application/application.tsx
@@ -3,17 +3,21 @@ export const Application = () => {
 		<>
 			<h1>Job application form</h1>
 			<h2>Heading 1</h2>
+			<p>All fields are mandatory</p>
+			<span title="close">x</span>
+			<img src="https://via.placeholder.com/150" alt="a person with a laptop" />
+			<div data-testid="custom-element">Custom HTML Element</div>
 			<form>
 				<div>
 					<label htmlFor="name">Name</label>
-					<input type="text" id="name" />
+					<input type="text" id="name" placeholder="Full Name" value="Abhishek" onChange={() => {}} />
 				</div>
 				<div>
 					<label htmlFor="bio">Bio</label>
 					<input type="text" id="bio" />
 				</div>
 				<div>
-					<label htmlFor="job-location">Job location</label>
+					<label htmlFor="job-location">Name</label>
 					<select id="job-location">
 						<option value="">Select a country</option>
 						<option value="US">Unites states</option>
